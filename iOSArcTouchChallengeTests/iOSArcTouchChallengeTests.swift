c6274f328df1bc9e97a4bfc6b10c7e64f8a7256e
Ricardo Sgobbe <sgobbericardo@gmail.com>
2018-11-11 23:22:42 -0200

    unit test added. Testing two features from datasource.

--- iOSArcTouchChallengeTests/iOSArcTouchChallengeTests.swift
+++ iOSArcTouchChallengeTests/iOSArcTouchChallengeTests.swift
@@ -6,22 +6,62 @@
 //  Copyright © 2018 Ricardo Sgobbe. All rights reserved.
 //
 
-@testable import Pods_iOSArcTouchChallenge
+@testable import iOSArcTouchChallenge
 
 import Quick
 import Nimble
 
-
-@testable import iOSArcTouchChallenge
-
 class iOSArcTouchChallengeTests: QuickSpec {
-
-    override func spec() {
-        let datasource = MovieDataSource()
-        datasource.fetchUpcomingMovie(page: 1) { (movie, error) in
-            expect(movie).notTo(beNil())
-            expect(error).notTo(beNil())
+    
+    class FakeDataSource: MovieDatasourceProtocol {
+        func getApiConfiguration(completion: @escaping (iOSArcTouchChallenge.Configuration?, Error?) -> Void) {
+            completion(nil, nil)
         }
+        
+        func fetchGenreList(completion: @escaping ([Genre]?, Error?) -> Void) {
+            let genre: [Genre] = []
+            completion(genre, nil)
+        }
+        
+        func fetchUpcomingMovie(page: Int, completion: @escaping (MovieResponse?, Error?) -> Void) {
+            
+        }
+        
+        func getMovieDetailsWith(id: Int, completion: @escaping (MovieDetail?, Error?) -> Void) {
+            
+        }
+        
+        func search(movie: String, completion: @escaping (MovieResponse?, Error?) -> Void) {
+            
+        }
+        
+        
+    }
+    
+    override func spec() {
+       let repository = MovieRepository()
+        repository.datasource = FakeDataSource()
+        describe("Validating datasource") {
+            
+            context("testing getConfig object", closure: {
+                it("should be nil", closure: {
+                    repository.downloadConfigObj(completion: { (config, error) in
+                        expect(config).to(beNil())
+                    })
+                })
+            })
+            
+            
+            context("testing getGenreList", closure: {
+                it("should return a empty array", closure: {
+                    repository.downloadGenreList(completion: { (genres, error) in
+                        expect(genres).to(beEmpty())
+                    })
+                })
+            })
+            
+        }
+        
     }
 
 }
